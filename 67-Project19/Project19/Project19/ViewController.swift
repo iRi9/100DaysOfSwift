2dd1685147120f93bc94345d7a3ee392d84986fe
_giri <giribahari45@gmail.com>
2019-07-17 06:41:07 +0700

    project20 part 2

---
+++ 67-Project19/Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by ebdesk-066 on 13/07/19.
+//  Copyright © 2019 irig.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
